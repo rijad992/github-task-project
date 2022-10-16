17d0d2efcf9749b2eef6d9fca92558bad72d6a8a
Rijad <rijad.lendo12@gmail.com>
2022-10-16 17:54:37 +0200

    Run pulumi from bash script

--- infra/infra-provision.sh
+++ infra/infra-provision.sh
@@ -18,5 +18,6 @@ pulumi config set --secret github_access_token $GH_ACCESS_TOKEN
 
 echo "Run pulumi command..."
 
-if [["$COMMAND" == "up"]]; then $UP_COMMAND_ARGS="--skip-preview"
-pulumi $COMMAND $UP_COMMAND_ARGS
+if [["$COMMAND" == "up"]]
+then $UP_COMMAND_ARGS="--skip-preview"
+else pulumi $COMMAND $UP_COMMAND_ARGS
