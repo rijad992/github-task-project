eeaa4073639db19d9e3144770c96cef8b9c344e3
Rijad <rijad.lendo12@gmail.com>
2022-10-17 10:47:47 +0200

    helmet config

--- src/core/app/helmetMiddleware.ts
+++ src/core/app/helmetMiddleware.ts
@@ -1,5 +1,4 @@
 import helmet from 'helmet';
-import crypto from 'crypto';
 import { NextFunction, Request, Response } from 'express';
 
 export const helmetMiddleware = (
@@ -7,24 +6,10 @@ export const helmetMiddleware = (
   response: Response,
   next: NextFunction,
 ): void => {
-  const nonce = crypto.randomBytes(16).toString('hex');
-  const contentSecurityPolicyDirectives = {
-    'default-src': ["'self'", 'http:'],
-    'base-uri': ["'self'"],
-    'block-all-mixed-content': [],
-    'font-src': ["'self'", 'http:', 'data:'],
-    'frame-ancestors': ["'self'"],
-    'img-src': ["'self'", 'http:', 'data:'],
-    'object-src': ["'none'"],
-    'script-src': ["'self'", 'http:', "'unsafe-inline'", `'nonce-${nonce}'`],
-    'script-src-attr': ["'none'"],
-    'style-src': ["'self'", 'http:', "'unsafe-inline'"],
-    'require-trusted-types-for': ["'script'"],
-  };
-  request.app.nonce = nonce;
+  const cspDefaults = helmet.contentSecurityPolicy.getDefaultDirectives();
+  delete cspDefaults['upgrade-insecure-requests'];
   helmet({
-    contentSecurityPolicy: { directives: contentSecurityPolicyDirectives },
+    contentSecurityPolicy: { directives: cspDefaults },
     crossOriginResourcePolicy: false,
-
   })(request, response, next);
 };
