34848c8c82f7adf196e47265122b6ea03dd89db6
Rijad Lendo <39672277+rijad992@users.noreply.github.com>
2022-10-17 11:35:42 +0200

    Pulumi secrets (#1)
    * Run pulumi from bash script

---
+++ infra/infra-provision.sh
@@ -0,0 +1,26 @@
+#! usr/bin/bash
+
+COMMAND=$1
+STACK=$2
+
+COMMAND_ARGS=""
+
+echo "Installing pulumi..."
+curl -fsSL https://get.pulumi.com | sh
+
+echo "Set stack..."
+
+pulumi stack select $STACK
+
+echo "Setting pulumi config...."
+
+pulumi config set --secret github_access_token $GH_ACCESS_TOKEN
+
+echo "Run pulumi command..."
+
+if [ $COMMAND = "up" ]
+then 
+COMMAND_ARGS="--yes"
+fi
+
+pulumi $COMMAND $COMMAND_ARGS
